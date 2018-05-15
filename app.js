ddf10e4bd897e32cf5573239b87e31db9336b10f
MichaelAllain <michaeldallain@gmail.com>
2018-05-15 14:53:00 -0700

    Add basic program structure and flow
    Created functions to handle page load,
    location form submission, map loading,
    station selection, date selection,
    AJAX request for tides.

--- app.js
+++ app.js
@@ -1,2 +1,90 @@
 "use strict";
-console.log("hello world");
+
+
+function processJSONTides(data) {
+    $.each(data.predictions, function(index, value){
+        if (value.v < 0) {
+            console.log(`Date: ${value.t}   Tide Height (ft.): ${value.v}`);
+        }
+    });
+    
+}
+
+ // submit AJAX request to NOAA
+function getTides(stationId, beginDate, endDate) {
+    console.log("Getting tides...");
+
+    const data = {
+        station: stationId,
+        begin_date: beginDate,
+        end_date: endDate,
+        product: "predictions",
+        datum: "MLLW",
+        units: "english",
+        time_zone: "lst_ldt",
+        format: "json",
+        interval: "hilo"
+    }
+
+    $.getJSON("https://tidesandcurrents.noaa.gov/api/datagetter", data, processJSONTides);
+}
+
+
+function loadDateFilters(stationId, stationName) {
+    
+    // render date selection screen
+    console.log(`Rendering date filter screen for ${stationName}`);
+    
+    // listen for date selection
+    console.log("Listening for date selection...");
+    
+        // temporary static dates for testing
+        const beginDate = 20180601;
+        const endDate = 20180901;
+        console.log(`Begin Date: ${beginDate}  End Date: ${endDate}`);
+        
+        // hide date selection screen
+        getTides(stationId, beginDate, endDate);
+
+}
+
+function handleTideStationSelection() {
+    // listen for tide station selection
+    console.log("Listening for station selection...");
+    
+        // Temporary static id and name for testing
+        const stationId = 9439026;    
+        const stationName = "Astoria (Youngs Bay), Oreg."
+        console.log(`Selected Station: ${stationName} (${stationId})`);
+
+        // hide map
+        console.log("Hiding Map..."); // $(#map).hide();
+
+        // load date filter selection page
+        loadDateFilters(stationId, stationName);
+}
+
+function loadMap(location) {
+    // load map
+    console.log("Map loading...");
+
+    // listen for user selection of tide station
+    handleTideStationSelection();
+}
+
+function handleLocationFormSubmit() {
+    $("#location-form").submit(function (evt) {
+        evt.preventDefault();
+        let location = $("#location-input").val();
+        console.log(`User location input: ${location}`);
+        // clear user input
+        $("#location-input").val("");
+        // hide location form
+        $("#location-form").hide();
+        //load map
+        loadMap(location);
+    });
+}
+
+// on document ready, handle location form submission
+$(handleLocationFormSubmit);
